84e51bbf79d4bd4b8be86bed80c1faba2ff6abf4
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-27 19:41:49 +0800

    changed opening function

--- square_cube_integers.py
+++ square_cube_integers.py
@@ -1,8 +1,7 @@
 #Write a method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers. The first output file will be named double.txt containing the square of all even integers found in integers.txt and the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt.
-
 #def process for opening file
-with open("integers.txt") as integer_list:
-    integers = integer_list.read().split()
+with open ("integers.txt", "r") as integer_file:
+    integers = list(map(int, integer_file.read().split()))
 #compute int squared
 def square(x):
     return x **2
