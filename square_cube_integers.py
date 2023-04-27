e350e79dbb069afdf5ba7da756cbaeb47bb89d30
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-27 19:36:48 +0800

    add empty list for even and odd

--- square_cube_integers.py
+++ square_cube_integers.py
@@ -1,7 +1,8 @@
 #Write a method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers. The first output file will be named double.txt containing the square of all even integers found in integers.txt and the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt.
 
 #def process for opening file
-
+with open("integers.txt") as integer_list:
+    integers = integer_list.read().split()
 #compute int squared
 def square(x):
     return x **2
@@ -9,9 +10,10 @@ def square(x):
 def cube(x):
     return x** 3
 #create list for both even & odd
-
+even=[]
+odd=[]
 #check if even or odd
-    #if even, add to even list
-    #if odd, add to odd list
+
+
 
 #make text files for both even and odd.
