9c03fa28beb9617bd2200aa19f3b27947c742d2f
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-27 19:28:21 +0800

    pseudocode

---
+++ square_cube_integers.py
@@ -0,0 +1,15 @@
+#Write a method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers. The first output file will be named double.txt containing the square of all even integers found in integers.txt and the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt.
+
+#def process for opening file
+
+#define int squared
+
+#define int cubed
+
+#create list for both even & odd
+
+#check if even or odd
+    #if even, add to even list
+    #if odd, add to odd list
+
+#make text files for both even and odd.
